e9e9ef609a6079bf8055ce6f4baf2c3e35d2574c
조, 광석 <asad1220@hanmail.net>
2026-04-07 18:04:34 +0900

    init

---
+++ BillIngredientSource/Init.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace BillIngredientSource {
+	[StaticConstructorOnStartup]
+	public static class Init {
+		static Init() {
+			var harmony = new Harmony("you.billingredientsource");
+			harmony.PatchAll();
+
+			Log.Message("[BillIngredientSource] Loaded");
+		}
+	}
+}
