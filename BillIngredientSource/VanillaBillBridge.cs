d2d8dfccc628dc50aca9183f652d06468319a73f
조, 광석 <asad1220@hanmail.net>
2026-04-10 00:25:12 +0900

    v1.1.1

--- BillIngredientSource/VanillaBillBridge.cs
+++ BillIngredientSource/VanillaBillBridge.cs
@@ -1,12 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using HarmonyLib;
 using RimWorld;
 using Verse;
 
 namespace BillIngredientSource {
 	public static class VanillaBillBridge {
-		[HarmonyReversePatch]
-		[HarmonyPatch(typeof(WorkGiver_DoBill), "TryFindBestBillIngredientsInSet")]
+		private delegate bool TryFindBestBillIngredientsInSetDelegate(
+			List<Thing> availableThings,
+			Bill bill,
+			List<ThingCount> chosen,
+			IntVec3 rootCell,
+			bool alreadySorted,
+			List<IngredientCount> missingIngredients
+		);
+
+		private static readonly TryFindBestBillIngredientsInSetDelegate tryFindBestBillIngredientsInSet;
+
+		static VanillaBillBridge() {
+			var method = AccessTools.Method(
+				typeof(WorkGiver_DoBill),
+				"TryFindBestBillIngredientsInSet",
+				new Type[] {
+					typeof(List<Thing>),
+					typeof(Bill),
+					typeof(List<ThingCount>),
+					typeof(IntVec3),
+					typeof(bool),
+					typeof(List<IngredientCount>)
+				}
+			);
+
+			if (method == null) {
+				throw new Exception("[BillIngredientSource] Failed to find WorkGiver_DoBill.TryFindBestBillIngredientsInSet");
+			}
+
+			tryFindBestBillIngredientsInSet =
+				AccessTools.MethodDelegate<TryFindBestBillIngredientsInSetDelegate>(method);
+		}
+
 		public static bool TryFindBestBillIngredientsInSet(
 			List<Thing> availableThings,
 			Bill bill,
@@ -14,7 +46,15 @@ namespace BillIngredientSource {
 			IntVec3 rootCell,
 			bool alreadySorted,
 			List<IngredientCount> missingIngredients) {
-			throw new System.NotImplementedException("Harmony reverse patch stub");
+
+			return tryFindBestBillIngredientsInSet(
+				availableThings,
+				bill,
+				chosen,
+				rootCell,
+				alreadySorted,
+				missingIngredients
+			);
 		}
 	}
 }
